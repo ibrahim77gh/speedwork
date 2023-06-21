64a1481daeddbf8a4a55148b4df75d60d2dc4c37
ibrahim77gh <syedibrahim4091@gmail.com>
2023-06-21 22:28:57 +0500

    Commit

--- src/Redux/baseUrl.js
+++ src/Redux/baseUrl.js
@@ -1,3 +1,3 @@
 // export const baseUrl = "http://localhost:3001/v1";
-// export const baseUrl = "https://speedllc-jvvz.vercel.app/v1"
-export const baseUrl = "http://43.205.138.230:3001/v1"
+export const baseUrl = "https://speedllc-jvvz.vercel.app/v1"
+// export const baseUrl = "http://43.205.138.230:3001/v1"
